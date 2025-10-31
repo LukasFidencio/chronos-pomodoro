98db91da6a9e0ada27cb623e477214370f2455b1
Lukas Fidencio <lukasfidencio.dev@gmail.com>
2025-10-31 16:50:53 -0300

    31/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Teste de children
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
     </>
   );
 }
