c3b8953b11558958e8b5c5567d8aceb10814181e
Lukas Fidencio <lukasfidencio.dev@gmail.com>
2025-11-18 08:31:45 -0300

    18/11

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import { initialTaskState } from "./initialTaskState";
-import { TaskContext } from "./TaskContext";
+import { useEffect, useReducer, useState } from 'react';
+import { initialTaskState } from './initialTaskState';
+import { TaskContext } from './TaskContext';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,13 +9,25 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+  const [numero, dispatch] = useReducer((state, action) => {
+    console.log(state, action)
+
+    switch (action){
+      case 'INCREMET':
+        return state + 1;
+    }
+
+    return state;
+  }, 0);
+
+  // useEffect(() => {
+  //   console.log(state);
+  // }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      <h1>o numero é: {numero}</h1>
+      <button onClick={()=> dispatch('INCREMENT')}>incrementar</button>
     </TaskContext.Provider>
   );
 }
